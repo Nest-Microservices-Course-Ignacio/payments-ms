6afdce821de1647ab35551280602a8efdd39cf59
nachomartinez3010 <nachomartinez3010@gmail.com>
2025-04-13 14:12:12 -0600

    Enhance microservices support: Add NATS configuration to environment variables, update main.ts to connect microservice using NATS transport, and include necessary dependencies in package.json and package-lock.json.

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -6,6 +6,7 @@ interface EnvVars {
   STRIPE_SECRET_KEY: string;
   API_GATEWAY: string;
   WEBHOOK_SECRET: string;
+  NATS_SERVERS: string[];
 }
 
 const envVarsSchema = joi
@@ -14,11 +15,15 @@ const envVarsSchema = joi
     STRIPE_SECRET_KEY: joi.string().required(),
     API_GATEWAY: joi.string().required(),
     WEBHOOK_SECRET: joi.string().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true)
   .required();
 
-const result = envVarsSchema.validate(process.env);
+const result = envVarsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS.split(','),
+});
 const { error, value } = result as {
   error?: joi.ValidationError;
   value: EnvVars;
@@ -31,6 +36,7 @@ export const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   stripeSecretKey: envVars.STRIPE_SECRET_KEY,
-  apiGateway: envVars.API_GATEWAY,/* TODO: change this */
+  apiGateway: envVars.API_GATEWAY /* TODO: change this */,
   webhookSecret: envVars.WEBHOOK_SECRET,
+  natsServers: envVars.NATS_SERVERS,
 };
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
-import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  /* this is a hybrid MS service */
+
   const app = await NestFactory.create(AppModule, {
     rawBody: true,
   });
 
-  app.setGlobalPrefix('/api');
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.NATS,
+    options: {
+      servers: envs.natsServers,
+      reconnect: true,
+    },
+  });
+
+  await app.startAllMicroservices();
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -18,6 +29,7 @@ async function bootstrap() {
     }),
   );
   await app.listen(envs.port);
+  
   Logger.log(`Starting server on port ${envs.port}`, 'Payments Service');
 }
 bootstrap();
